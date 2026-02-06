90801245f8ab128d1a242475bd62265e1323b50f
Mohd Kashif <145757240+NupurSrivasatava@users.noreply.github.com>
2026-02-06 11:35:14 +0530

    update

--- HRMS/backend/main.py
+++ HRMS/backend/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "http://127.0.0.1:5175",
-        "https://hrmskashif.netlify.app"
+        "https://hrmsnupur.netlify.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
